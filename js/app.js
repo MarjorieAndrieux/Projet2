886e78152e154c4383d1f2512c918f24e70ef93c
MarjorieAndrieux <33328367+MarjorieAndrieux@users.noreply.github.com>
2017-11-09 20:35:39 +0100

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,26 @@
+function addition(a,b){
+	var result=a+b;
+}
+
+function soustraction (a,b) {
+	var result=a-b;
+}
+
+function multiplication (a,b) {
+	var result=a*b;
+}
+
+function division (a,b) {
+	if (b<=0) {
+		result ("Impossible de diviser par 0");
+	}
+	else (){
+		var result=a/b;
+	}
+ }
+
+ function result() {
+ 	if () {
+ 		
+ 	}
+ }
